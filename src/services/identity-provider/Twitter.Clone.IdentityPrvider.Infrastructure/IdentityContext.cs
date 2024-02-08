406cc922861ec67a352d57647c146c04abb241ab
javidleo <javidleo.ef@gmail.com>
2024-02-08 22:54:28 +0330

    add identity provider solution to the repo

---
+++ src/services/identity-provider/Twitter.Clone.IdentityPrvider.Infrastructure/IdentityContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Twitter.Clone.IdentityPrvider.Infrastructure;
+
+public class IdentityContext(DbContextOptions<IdentityContext> options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}

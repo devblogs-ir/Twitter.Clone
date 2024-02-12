5979b0bea6d04c24b14d5b5e849471b01e18c85c
AREZOO <AREZOO@DESKTOP-RTIJ4O7>
2024-02-12 15:17:48 +0330

    remove mediator from initial setup

--- src/services/search-engine/Twitter.Clone.Search.Application/ConfigureServices.cs
+++ src/services/search-engine/Twitter.Clone.Search.Application/ConfigureServices.cs
@@ -8,10 +8,6 @@ public static class ConfigureServices
 {
     public static IServiceCollection RegisterApplicationServices(this IServiceCollection services)
     {
-        services.AddMediatR(cfg =>
-        {
-            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-        });
 
         return services;
     }
--- src/services/search-engine/Twitter.Clone.Search.Presentation/Program.cs
+++ src/services/search-engine/Twitter.Clone.Search.Presentation/Program.cs
@@ -1,35 +1,28 @@
 
-namespace Twitter.Clone.Search.Presentation;
+    var builder = WebApplication.CreateBuilder(args);
 
-public class Program
-{
-    public static void Main(string[] args)
+    // Add services to the container.
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
-        app.Run();
+        app.UseSwagger();
+        app.UseSwaggerUI();
     }
-}
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+
+    app.MapControllers();
+
+    app.Run();
+

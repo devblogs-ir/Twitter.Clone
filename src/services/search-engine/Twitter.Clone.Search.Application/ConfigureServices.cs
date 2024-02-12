e7dede6ef0ba42372b4e1e257eb3c86bd5275f4e
AREZOO <AREZOO@DESKTOP-RTIJ4O7>
2024-02-12 13:02:58 +0330

    Search init project setup

---
+++ src/services/search-engine/Twitter.Clone.Search.Application/ConfigureServices.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Twitter.Clone.Search.Application;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection RegisterApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
+        return services;
+    }
+}
---
+++ src/services/search-engine/Twitter.Clone.Search.Presentation/ConfigureServices.cs
@@ -0,0 +1,10 @@
+﻿namespace Twitter.Clone.Search.Presentation;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection RegisterPresentationServices(this IServiceCollection services)
+    {
+        return services;    
+    }
+
+}

2c07bb0ef6fc67692b86f9b7251e42a1a3ef92dd
AREZOO <AREZOO@DESKTOP-RTIJ4O7>
2024-02-19 10:02:17 +0330

    Add project to base solution

---
+++ Twitter.Clone.Search.WebApi/Entities/Tweet.cs
@@ -0,0 +1,16 @@
+﻿namespace Twitter.Clone.Search.WebApi.Entities;
+
+public class Tweet
+{
+    public Guid Id { get; init; }
+    public Guid UserId { get; init; }
+
+    public string Content { get; init; }
+    public int LikeCount { get; init; }
+    public DateTime CreationDate { get; init; }
+    public int RetweetCount { get; init; }
+
+    public List<TweetHashtag> TweetHashtags { get;  } = [];
+    public List<Hashtag> Hashtags { get; } = [];
+
+}

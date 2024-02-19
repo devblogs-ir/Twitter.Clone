a41e23ec9a89856b22239d2ba7bc3a747fca9bef
AREZOO <AREZOO@DESKTOP-RTIJ4O7>
2024-02-19 10:43:30 +0330

    Remove Unnecssary files forn init project

--- Twitter.Clone.Search.WebApi/Context/ApplicationDbContext.cs
+++ Twitter.Clone.Search.WebApi/Context/ApplicationDbContext.cs
@@ -6,16 +6,13 @@ namespace Twitter.Clone.Search.WebApi.Context;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
-    public DbSet<Tweet> Tweets { get; set; }
-    public DbSet<TweetHashtag> TweetHashtags { get; set; }
-    public DbSet<Hashtag> Hashtags { get; set; }
+ 
 
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
     }
 
 }

773645c5fbd353b336313fb2c050fc92b9de2cb4
Nabi Karampoor <thisisnabi@outlook.com>
2024-02-22 19:24:30 +0330

    feat: Add location domain model

---
+++ src/services/locator/Twitter.Clone.Locator/Domain/Location.cs
@@ -0,0 +1,23 @@
+﻿namespace Twitter.Clone.Locator.Domain;
+
+public class Location
+{
+    public int Id { get; set; }
+
+    public double Latitude { get; set; } 
+
+    public double Longitude { get; set; }
+
+    public required string ContinentName { get; set; }
+    
+    public required string CountryName { get; set; }
+
+    public required string CountryCode { get; set; }
+
+    public required string State { get; set; }
+    
+    public required string City { get; set; }
+
+    public required string CallingCode { get; set; }
+
+}

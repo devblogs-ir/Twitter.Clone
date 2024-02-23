6b7a7afa9eb45b33d2020c0dab82784e0fec036b
Davood Baghdadi <davobag@yahoo.com>
2024-02-23 14:11:22 +0330

    create message entity

---
+++ src/services/messenger/Twitter.Clone.Messenger/Domain/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace Twitter.Clone.Messenger.Domain
+{
+    public class Message
+    {
+        public long MessageId { get; set; }
+
+        public string? MessageBody {  get; set; }
+    }
+}

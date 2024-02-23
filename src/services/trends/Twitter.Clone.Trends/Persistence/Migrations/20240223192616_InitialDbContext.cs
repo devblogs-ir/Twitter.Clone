c82e1e04b8746d3abc7315b301cf3a0a3d3f93e5
Nabi Karampoor <thisisnabi@outlook.com>
2024-02-23 22:58:32 +0330

    feat: Add init database migration into the trend project

---
+++ src/services/trends/Twitter.Clone.Trends/Persistence/Migrations/20240223192616_InitialDbContext.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Twitter.Clone.Trends.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "trends");
+
+            migrationBuilder.CreateTable(
+                name: "HashTags",
+                schema: "trends",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HashTags", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HashTagEntries",
+                schema: "trends",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HashTagId = table.Column<long>(type: "bigint", nullable: false),
+                    Processed = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HashTagEntries", x => new { x.HashTagId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_HashTagEntries_HashTags_HashTagId",
+                        column: x => x.HashTagId,
+                        principalSchema: "trends",
+                        principalTable: "HashTags",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HashTags_Name",
+                schema: "trends",
+                table: "HashTags",
+                column: "Name",
+                unique: true)
+                .Annotation("SqlServer:Clustered", false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HashTagEntries",
+                schema: "trends");
+
+            migrationBuilder.DropTable(
+                name: "HashTags",
+                schema: "trends");
+        }
+    }
+}

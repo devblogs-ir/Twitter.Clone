514029927b5a07720d3761a1329cadfb98f9b02f
hamidreza <hamidreza.rahmani99@gmail.com>
2024-02-28 10:57:51 -0500

    Add MassTransit Package and Configure BrokerService

--- src/services/notifier/Twitter.Clone.Notifier.Host/Program.cs
+++ src/services/notifier/Twitter.Clone.Notifier.Host/Program.cs
@@ -1,7 +1,10 @@
+using Twitter.Clone.Notifier.Host.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.ConfigureBrokerService(builder.Configuration);
+builder.Services.ConfigureAppSettings(builder.Configuration);
 
 var app = builder.Build();
-
 
 app.Run();

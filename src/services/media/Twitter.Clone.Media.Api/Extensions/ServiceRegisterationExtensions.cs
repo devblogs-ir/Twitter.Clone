b0275f879ad42850cfd282c277d24b8d4886e537
Hassan Taherian <hassantsf.dev@gmail.com>
2024-02-29 12:46:02 +0330

    feat: add MassTransit for messaging and RabbitMQ integration

---
+++ src/services/media/Twitter.Clone.Media.Api/Extensions/ServiceRegisterationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using Twitter.Clone.Media.Api.Configurations;
+
+namespace Twitter.Clone.Media.Api.Extensions;
+
+public static class ServiceRegisterationExtensions
+{
+    public static void AddMessageBroker(this IServiceCollection services)
+    {
+        var rabbitMqAddress = new Uri("amqp://localhost:5672");
+
+        services.AddMassTransit(configure =>
+        {
+            // Consumer Registration
+            configure.UsingRabbitMq((context, config) =>
+            {
+                config.Host(rabbitMqAddress, "/", configure =>
+                {
+                    configure.Username("guest");
+                    configure.Password("guest");
+                });
+
+                config.ConfigureEndpoints(context);
+            });
+        });
+    }
+}
--- src/services/media/Twitter.Clone.Media.Api/Program.cs
+++ src/services/media/Twitter.Clone.Media.Api/Program.cs
@@ -1,9 +1,10 @@
+using Twitter.Clone.Media.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMessageBroker();
 
 var app = builder.Build();
 

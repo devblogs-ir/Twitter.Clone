0e92d75dac87214697143d7e473ab7d33bd2c162
bhata <b_hatamloo@yahoo.com>
2024-03-03 20:40:18 +0300

    add farapayamak setting class and appsetting

---
+++ src/services/notifier/Twitter.Clone.Notifier.Features.Sms/Settings/FarapayamakSetting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twitter.Clone.Notifier.Features.Sms.Settings
+{
+    public class FarapayamakSetting
+    {
+        public const string SectionName = "FarapayamakSetting";
+        public string ApiUrl { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string SenderNumber { get; set; }
+    }
+}

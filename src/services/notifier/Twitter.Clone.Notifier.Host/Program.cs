feff0f75bb637bce3baf7332b0899779a1a54f0f
bhata <b_hatamloo@yahoo.com>
2024-03-03 20:41:42 +0300

    add option pattern to farapayamak setting

--- src/services/notifier/Twitter.Clone.Notifier.Host/Program.cs
+++ src/services/notifier/Twitter.Clone.Notifier.Host/Program.cs
@@ -1,7 +1,9 @@
+using Twitter.Clone.Notifier.Features.Sms.Settings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 var app = builder.Build();
-
+builder.Services.Configure<FarapayamakSetting>(builder.Configuration.GetSection(FarapayamakSetting.SectionName));
 
 app.Run();

234b796cce7c1de7dce6c9566fc1fa4482536397
bhata <b_hatamloo@yahoo.com>
2024-03-03 20:45:39 +0300

    add sma service contract

---
+++ src/services/notifier/Twitter.Clone.Notifier.Features.Sms/Services/SmsService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twitter.Clone.Notifier.Features.Sms.Contracts;
+using Twitter.Clone.Notifier.Features.Sms.Settings;
+
+namespace Twitter.Clone.Notifier.Features.Sms.Services
+{
+    public class SmsService : ISmsService
+    {
+        private readonly IOptions<FarapayamakSetting> _farapayamakOptions;
+        public SmsService(IOptions<FarapayamakSetting> farapayamakOptions)
+        {
+            _farapayamakOptions = farapayamakOptions;
+        }
+        public async Task<bool> Send(string messageBody, string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

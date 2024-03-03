20af37faf58451e749791413e7a4a378646fce54
Davood Baghdadi <davobag@yahoo.com>
2024-03-03 07:53:29 +0330

    add public chat controller

---
+++ src/services/messenger/Twitter.Clone.Messenger/Controllers/PublicChatController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Twitter.Clone.Messenger.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class PublicChatController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+                        await Task.CompletedTask;
+            return Ok();
+        }
+    }
+}

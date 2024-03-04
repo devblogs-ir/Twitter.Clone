1dc59468c78f9259bb52f673bef8091a10da7074
bhata <b_hatamloo@yahoo.com>
2024-03-04 11:29:51 +0300

    refactor sms setting for multiple providers

--- src/services/notifier/Twitter.Clone.Notifier.Features.Sms/Services/SmsService.cs
+++ src/services/notifier/Twitter.Clone.Notifier.Features.Sms/Services/SmsService.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Twitter.Clone.Notifier.Common.ApiCaller;
+using Twitter.Clone.Notifier.Common.Enums;
 using Twitter.Clone.Notifier.Features.Sms.Contracts;
 using Twitter.Clone.Notifier.Features.Sms.Models;
 using Twitter.Clone.Notifier.Features.Sms.Settings;
@@ -14,24 +15,39 @@ namespace Twitter.Clone.Notifier.Features.Sms.Services
 {
     public class SmsService : ISmsService
     {
-        private readonly IOptions<FarapayamakSetting> _farapayamakOptions;
-        public SmsService(IOptions<FarapayamakSetting> farapayamakOptions)
+        private readonly IOptions<SmsProvidersSetting> _smsOptions;
+        public SmsService(IOptions<SmsProvidersSetting> smsOptions)
         {
-            _farapayamakOptions = farapayamakOptions;
+            _smsOptions = smsOptions;
         }
         public async Task<bool> Send(string messageBody, string phoneNumber)
         {
-            var requestModel = new FarapayamakRequestModel
+            var requestModel = new FarapayamakRequestModel();
+            requestModel.text = messageBody;
+            requestModel.to = phoneNumber;
+            if (_smsOptions.Value.ActiveSmsProvider == nameof(SmsProviders.Farapayamak))
             {
-                text = messageBody,
-                password = _farapayamakOptions.Value.Password,
-                to = phoneNumber,
-                from = _farapayamakOptions.Value.SenderNumber,
-                username = _farapayamakOptions.Value.UserName
-            };
-            var response = await ApiCaller.PostAsync(_farapayamakOptions.Value.ApiUrl, requestModel);
-            response.EnsureSuccessStatusCode();
-            return true;
+                requestModel.password = _smsOptions.Value.FarapayamakSetting.Password;
+                requestModel.username = _smsOptions.Value.FarapayamakSetting.UserName;
+                requestModel.from = _smsOptions.Value.FarapayamakSetting.SenderNumber;
+                var response = await ApiCaller.PostAsync(_smsOptions.Value.FarapayamakSetting.ApiUrl, requestModel);
+                response.EnsureSuccessStatusCode();
+                return true;
+            }
+            else if (_smsOptions.Value.ActiveSmsProvider == nameof(SmsProviders.SmsIr))
+            {
+                requestModel.password = _smsOptions.Value.SmsIrSetting.Password;
+                requestModel.username = _smsOptions.Value.SmsIrSetting.UserName;
+                requestModel.from = _smsOptions.Value.SmsIrSetting.SenderNumber;
+                var response = await ApiCaller.PostAsync(_smsOptions.Value.SmsIrSetting.ApiUrl, requestModel);
+                response.EnsureSuccessStatusCode();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
     }
 }

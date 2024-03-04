d7c3aa04880db2eb5ee726f5ca278b9382b5e4fb
bhata <b_hatamloo@yahoo.com>
2024-03-04 10:02:13 +0300

    add a test api to host project for test service

--- src/services/notifier/Twitter.Clone.Notifier.Host/Program.cs
+++ src/services/notifier/Twitter.Clone.Notifier.Host/Program.cs
@@ -1,9 +1,11 @@
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Twitter.Clone.Notifier.Common.Autofac;
+using Twitter.Clone.Notifier.Features.Sms.Contracts;
 using Twitter.Clone.Notifier.Features.Sms.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<FarapayamakSetting>(builder.Configuration.GetSection(FarapayamakSetting.SectionName));
 builder.Host
     .UseServiceProviderFactory(new AutofacServiceProviderFactory())
     .ConfigureContainer<ContainerBuilder>((container) =>
@@ -13,4 +15,9 @@ builder.Host
 
 var app = builder.Build();
 
+app.MapPost("/test", (string text, string mobile
+    , ISmsService smsService) =>
+{
+    smsService.Send(text, mobile);
+});
 app.Run();

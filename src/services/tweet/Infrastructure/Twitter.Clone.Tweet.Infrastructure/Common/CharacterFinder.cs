611f95cce1b7bd47ea286afb32228a106e21cf2d
fzleli <amirfzlelii@gmail.com>
2024-03-05 22:39:31 +0330

    feat: Add character finder class to find hashtag  into the tweet project

---
+++ src/services/tweet/Infrastructure/Twitter.Clone.Tweet.Infrastructure/Common/CharacterFinder.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Twitter.Clone.Tweet.Infrastructure.Common
+{
+    public class CharacterFinder
+    {
+        public static List<string> FindWordsWith(char character, string input)
+        {
+            List<string> tags = new List<string>();
+            string pattern = $@"{character}\w+";
+
+            Regex regex = new Regex(pattern, RegexOptions.IgnoreCase);
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                tags.Add(match.Value.Substring(1));
+            }
+
+            return tags;
+        }
+    }
+}

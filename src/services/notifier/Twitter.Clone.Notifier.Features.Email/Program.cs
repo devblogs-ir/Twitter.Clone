0d629e060f1ff9a82c09faba7b8effc19d51f348
zahra liaghatmand <zahra.liaghatmand@gmail.com>
2024-03-10 18:42:12 +0330

    add Reading MailSettings
    this is #105 Adding Configuration to MailSettings class at Runtime

--- src/services/notifier/Twitter.Clone.Notifier.Features.Email/Program.cs
+++ src/services/notifier/Twitter.Clone.Notifier.Features.Email/Program.cs
@@ -1,7 +1,11 @@
+using Twitter.Clone.Notifier.Features.Email.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
 
 var app = builder.Build();
 

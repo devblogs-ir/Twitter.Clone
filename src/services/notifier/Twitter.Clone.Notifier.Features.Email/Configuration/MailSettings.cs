34b236379a4c9c082c422e7a8c0969c06c766acd
zahra liaghatmand <zahra.liaghatmand@gmail.com>
2024-03-10 18:37:06 +0330

    Add  MailSettings
    this is #105

---
+++ src/services/notifier/Twitter.Clone.Notifier.Features.Email/Configuration/MailSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Twitter.Clone.Notifier.Features.Email.Configuration
+{
+    public class MailSettings
+    {
+        public string Server { get; set; } = string.Empty;
+        public short port { get; set; }
+        public string SenderName { get; set; } = string.Empty;
+        public string SenderEmail { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
